4f4666dfa486c995bb43089467f5e8f71797e68b
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-02 16:36:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,24 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  lowest_vaule = v
+  lowest_key = k
+  name_hash.each do |k,v|
+    if v <= 1
+      return k
+    else
+      nil
+    end
+  k
+end
+  # lowest_key = nil
+  # highest_key = nil
+  # hash.each do |v,k|
+  #   if lowest_vaule == nil || v < lowest_vaule
+  #     lowest_vaule = v
+  #     lowest_key = k
+  #   end
+  # end
+  # lowest_key
 
 end
